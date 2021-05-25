be81d55ea2071eade93e16cecbf92c9baf2682da
zwaml <66986407+zwaml@users.noreply.github.com>
2021-05-25 17:28:02 +0200

    Update authorize.py

--- bot/plugins/authorize.py
+++ bot/plugins/authorize.py
@@ -16,8 +16,8 @@ from bot.helpers.utils import CustomFilters
 OAUTH_SCOPE = "https://www.googleapis.com/auth/drive"
 REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
 G_DRIVE_DIR_MIME_TYPE = "application/vnd.google-apps.folder"
-G_DRIVE_CLIENT_ID = "202264815644.apps.googleusercontent.com"
-G_DRIVE_CLIENT_SECRET = "X4Z3ca8xfWDb1Voo-F9a7ZxJ"
+G_DRIVE_CLIENT_ID = "1000007788543-vdlrmhr4uf4i0392ft7hig3mapp7a637.apps.googleusercontent.com"
+G_DRIVE_CLIENT_SECRET = "dV1lRaCinwzY-im4hVxJbFSN"
 flow = None
 
 @Client.on_message(filters.private & filters.incoming & filters.command(BotCommands.Authorize))
