2b0b92572d5f2f513d871bbdac6f3b80a67764eb
zwaml <66986407+zwaml@users.noreply.github.com>
2021-05-25 16:49:35 +0200

    Update download.py

--- bot/plugins/download.py
+++ bot/plugins/download.py
@@ -13,7 +13,7 @@ from pyrogram.errors import FloodWait, RPCError
 def _download(client, message):
   user_id = message.from_user.id
   if not message.media:
-    sent_message = message.reply_text('🕵️**Checking link...**', quote=True)
+    sent_message = message.reply_text('🕵️**جاري فحص الرابط ...**', quote=True)
     if message.command:
       link = message.command[1]
     else:
@@ -49,7 +49,7 @@ def _download(client, message):
 @Client.on_message(filters.private & filters.incoming & (filters.document | filters.audio | filters.video) & CustomFilters.auth_users)
 def _telegram_file(client, message):
   user_id = message.from_user.id
-  sent_message = message.reply_text('🕵️**Checking File...**', quote=True)
+  sent_message = message.reply_text('🕵️**جاري فحص الملف ...**', quote=True)
   if message.document:
     file = message.document
   elif message.video:
@@ -72,7 +72,7 @@ def _telegram_file(client, message):
 def _ytdl(client, message):
   user_id = message.from_user.id
   if len(message.command) > 1:
-    sent_message = message.reply_text('🕵️**Checking Link...**', quote=True)
+    sent_message = message.reply_text('🕵️**جاري فحص الرابط ...**', quote=True)
     link = message.command[1]
     LOGGER.info(f'YTDL:{user_id}: {link}')
     sent_message.edit(Messages.DOWNLOADING.format(link))
